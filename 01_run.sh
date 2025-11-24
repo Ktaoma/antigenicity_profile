3348b5496fbfa77c01dd2b5541cee5d2f103b553
Ktaoma <37401205+Ktaoma@users.noreply.github.com>
2025-11-24 21:49:06 +0700

    Update 01_run.sh

--- 01_run.sh
+++ 01_run.sh
@@ -1,97 +1,75 @@
-#makeblastdb -in ../../../db/influenza/phase2/sequences_DNA.fasta -dbtype nucl
+#makeblastdb -in db/sequences_DNA.fasta -dbtype nucl
 
 
-i=$1
-type=$2
-leng=$3
+#Mmake result directory
+mkdir -p result/fastq
+mkdir -p result/stat
+mkdir -p result/tmp
+mkdir -p result/bam
+mkdir -p result/consensus
 
-#sn=$(echo $i | cut -d '/' -f7 | sed -e 's/.fastq.gz//g')
-mkdir result
-seqkit stat $i > result/sample.tsv
+#Summary statistics of raw fastq
+seqkit stat $1 > result/stat/summary_statistcs.tsv
+
+#Select file based on pattern
+seqkit locate -p AGTAGAAACAAGG $1 | gzip -9 > result/tmp/reverse.tsv.gz
+seqkit locate -p AGCAAAAGCAGG  $1 | gzip -9 > result/tmp/forward.tsv.gz
+
+#Select the read id with correct orientation
+Rscript ./script/02_parse_primer_pattern.R result/tmp both
+seqkit grep -f result/tmp/QC_id.tsv $1 | gzip -9 > result/fastq/filtered.fq.gz
+seqkit stat result/fastq/filtered.fq.gz | tail -n+2 >> result/stat/summary_statistcs.tsv
+rm -rf result/fastq/*.tsv.gz
+
+#'Trim primer'
+#cutadapt --revcomp -a AGCAAAAGCAGG...CCTTGTTTCTACT -m 800 -q 15 -o result/fastq/filtered_02.fq.gz result/fastq/filtered.fq.gz
+seqkit seq -m 500 -Q 10 result/fastq/filtered.fq.gz |\
+  seqkit subseq -r 50:-50 | gzip -9 > result/fastq/filtered_02.fq.gz
+seqkit stat result/fastq/filtered_02.fq.gz | tail -n+2 >> result/stat/summary_statistcs.tsv
+  
+# 'Sort reads into bucket'
+#seqkit fq2fa result/fastq/filtered_02.fq.gz |\
+#  blastn -query - -db db/sequences_DNA.fasta -outfmt '6 qseqid sseqid pident length qlen' > result/tmp/blast_hits.tsv
+#cat result/tmp/blast_hits.tsv | awk '{prop=100*($4/$5);print $0 "\t" prop}'| awk '$3 >= 80 && $6 >= 80' > result/tmp/blast_hits_filtered.tsv
 
 
-
-function h {
-  path=result/$sn/${type}
-mkdir -p $path
-
-echo 'Get summary statistic of uncurated file'
-seqkit stat $i -Ta | sed -e 's/,//g' >> ${path}/read_count.tsv
-
-echo 'Get primer pattern'
-pp=$path/pattern/$type
-mkdir -p $pp
-seqkit locate -m 2 -p AGTAGAAACAAGG $i | awk -v ss=$sn '{print $0,ss}' | gzip -9 > ${pp%/type}/reverse.tsv.gz
-seqkit locate -m 2 -p AGCAAAAGCAGG  $i | awk -v ss=$sn '{print $0,ss}' | gzip -9 > ${pp%/type}/forward.tsv.gz
-
-echo 'Extract primer'
-mkdir -p ${pp}
-Rscript ./02_parse_primer_pattern.R $pp $type
-
-echo 'Export as Fastq'
-seqkit grep -f $pp/QC_id.tsv $i | gzip -9 > $path/filtered.fq.gz
-
-echo 'Trim primer'
-cutadapt --revcomp -a AGCAAAAGCAGG...CCTTGTTTCTACT -e 0.25 -m $leng -q 10 -o $path/sample_final_qc.fq.gz $path/filtered.fq.gz
-seqkit fq2fa $path/sample_final_qc.fq.gz | gzip -9 > $path/sample_final.fa.gz
-
-echo 'Sort reads into bucket'
-zcat $path/sample_final.fa.gz | blastn -query - -db ../../../db/influenza/phase2/sequences_DNA.fasta -outfmt '6 qseqid sseqid pident length qlen' -word_size 28 > $path/blast_hits.tsv
-cat ${path}/blast_hits.tsv | awk '{prop=100*($4/$5);print $0 "\t" prop}'| awk '$3 >= 80 && $6 >= 80' > ${path}/blast_hits_filtered.tsv
-
-echo 'Get summary statistics of QC fasq'
-seqkit stat $path/sample_final.fa.gz -Ta | sed -e 's/,//g' | awk 'NR!=1 {print $0}' >> ${path}/read_count.tsv
-
-for k in 4;
+for k in 4 6;
 do
 
-    echo 'Get read of k segment'
-    cat ../../../db/influenza/phase2/sequences_DNA.fasta | grep ">" | grep "segment $k"  |\
-        awk '{print $1}' | sed 's/>//g' | grep -f - $path/blast_hits_filtered.tsv | cut -f1 | sort -u |\
-            seqkit grep -f - $path/sample_final.fa.gz | gzip -9 > $path/sample_final_segment_${k}.fa.gz
+   #'Get read of k segment'
+   cat db/sequences_DNA.fasta | grep ">" | grep "segment $k"  |\
+        awk '{print $1}' | sed 's/>//g' | grep -f - result/tmp/blast_hits_filtered.tsv | cut -f1 | sort -u |\
+            seqkit grep -f - result/fastq/filtered_02.fq.gz | gzip -9 > result/fastq/filtered_${k}.fq.gz
+            
+  
+   #'Get the name of best reference of k segment'
+   name=$(cat db/sequences_DNA.fasta | grep ">" |\
+            grep "segment $k" | awk '{print $1}' |\
+              sed 's/>//g' | grep -f - result/tmp/blast_hits_filtered.tsv |\
+                cut -f2 | sort | uniq -c | sort -k1,1nr | awk 'NR==1{print $2}')
+  
+    echo $name 
+    echo haha
+    #'Generating consensus'
+    seqkit fx2tab db/sequences_DNA.fasta | grep $name | seqkit tab2fx > result/tmp/best_ref.fa
+    seqkit fx2tab db/sequences_DNA.fasta | grep $name | seqkit tab2fx |\
+        minimap2 -a - result/fastq/filtered_${k}.fq.gz |\
+            samtools sort -o result/tmp/filtered_${k}.bam && samtools index result/tmp/filtered_${k}.bam
 
-    echo 'Get summary statistics of k segment'
-    seqkit stat $path/sample_final_segment_${k}.fa.gz -Ta | sed -e 's/,//g' | awk 'NR!=1 {print $0}' >> ${path}/read_count.tsv
-
-
-    echo 'Get the name of best reference of k segment'
-    name=$(cat ../../../db/influenza/phase2/sequences_DNA.fasta | grep ">" | grep "segment $k"  | awk '{print $1}' |\
-           sed 's/>//g' | grep -f - ${path}/blast_hits_filtered.tsv | cut -f2 | sort | uniq -c | sort -k1,1nr | awk 'NR==1{print $2}')
-
-
-    echo 'Remove supplmentary reads'
-    seqkit fx2tab ../../../db/influenza/phase2/sequences_DNA.fasta | grep $name | seqkit tab2fx |\
-        minimap2 -a - $path/sample_final_segment_${k}.fa.gz |\
-            samtools sort -o $path/sample_final_segment_${k}_tmp.bam && samtools index $path/sample_final_segment_${k}_tmp.bam
-
-    samtools view -f 2048  $path/sample_final_segment_${k}_tmp.bam | cut -f1 | sort -u > $path/supplement_${k}.txt
-    samtools view -h $path/sample_final_segment_${k}_tmp.bam | grep -vf $path/supplement_${k}.txt | samtools view -b -o $path/sample_final_segment_${k}.bam -
-
-
-    echo 'Remove read with excessive INDEL and softclip'
-    samtools view -h $path/sample_final_segment_${k}.bam | awk '$5==60 {print $1,$6}' > ${path}/ReadCIGAR.tsv
-
-    Rscript ./03_parse_CIGAR.R $path
-    mv ${path}/ReadCIGAR.tsv ${path}/ReadCIGAR_${k}.tsv
-    mv ${path}/CIGAR_Tab.tsv ${path}/CIGAR_Tab_${k}.tsv
-
-    cat ${path}/CIGAR_Tab_${k}.tsv  | awk '$10 != "keep" {print $1}' > $path/sample_final_segment_${k}_ID_filtered.txt
-    samtools view -h $path/sample_final_segment_${k}.bam | grep -vf $path/sample_final_segment_${k}_ID_filtered.txt | samtools view -b -o $path/sample_final_segment_${k}_filtered.bam -
-    samtools sort $path/sample_final_segment_${k}_filtered.bam -o $path/sample_final_segment_${k}_filtered_sorted.bam
-    samtools index $path/sample_final_segment_${k}_filtered_sorted.bam
-
-    #echo 'Reference based assembly'
-    samtools consensus -m simple -d 30 -A $path/sample_final_segment_${k}_filtered_sorted.bam |\
-           seqkit fx2tab | awk -v sample=$sn -v seg=$k 'BEGIN{OFS="\t"}{print sample "_" seg,$2}' |\
-                    seqkit tab2fx > $path/draft_segment_${k}.fasta
-
+    samtools sort result/tmp/filtered_${k}.bam -o result/bam/filtered_${k}_sorted.bam
+    samtools index result/bam/filtered_${k}_sorted.bam
+    
+    samtools consensus -m simple -d 1 -A result/bam/filtered_${k}_sorted.bam |\
+           seqkit fx2tab | awk -v sample="sample" -v seg=$k 'BEGIN{OFS="\t"}{print sample "_" seg,$2}' |\
+                    seqkit tab2fx > result/consensus/draft_segment_${k}.fasta
+    
     #echo 'Calculate depth'
-    samtools depth $path/sample_final_segment_${k}_filtered_sorted.bam |\
-           awk -v sample=$sn -v seg=$k '{print sample "_" seg,$2,$3}' > $path/depth_segment_${k}.tsv
-
-    #echo 'Self alignment'
-    samtools fastq $path/sample_final_segment_${k}_filtered_sorted.bam | gzip -9 > $path/sample_final_segment_${k}_filtered_sorted.fq.gz
-    minimap2 -a $path/draft_segment_${k}.fasta $path/sample_final_segment_${k}_filtered_sorted.fq.gz > $path/self_aln_${k}.sam
-    samtools sort $path/self_aln_${k}.sam -o $path/self_aln_${k}.bam
-done  
-}
+    samtools depth result/bam/filtered_${k}_sorted.bam |\
+           awk -v sample="sample" -v seg=$k '{print sample "_" seg,$2,$3}' > result/stat/depth_segment_${k}.tsv
+  
+    #echo 'sum stat'
+    seqkit stat result/fastq/filtered_${k}.fq.gz | tail -n+2 >> result/stat/summary_statistcs.tsv
+  
+  
+  
+done
